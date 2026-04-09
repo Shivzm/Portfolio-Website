3a0888542e37e2c918b9956c9744e818b68f5b3b
ShivammMukherjee <shibhammukherjee6@gmail.com>
2026-04-09 22:57:37 +0530

    Modified Components

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -1,9 +1,12 @@
-import '@fortawesome/free-regular-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBriefcase } from '@fortawesome/free-solid-svg-icons';
-import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
-import 'react-vertical-timeline-component/style.min.css';
-import '../assets/styles/Timeline.scss'
+import "@fortawesome/free-regular-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBriefcase } from "@fortawesome/free-solid-svg-icons";
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import "react-vertical-timeline-component/style.min.css";
+import "../assets/styles/Timeline.scss";
 
 function Timeline() {
   return (
@@ -13,41 +16,49 @@ function Timeline() {
         <VerticalTimeline>
           <VerticalTimelineElement
             className="vertical-timeline-element--work"
-            contentStyle={{ background: 'white', color: 'rgb(39, 40, 34)' }}
-            contentArrowStyle={{ borderRight: '7px solid  white' }}
-            iconStyle={{ background: '#5000ca', color: 'rgb(39, 40, 34)' }}
+            contentStyle={{ background: "white", color: "rgb(39, 40, 34)" }}
+            contentArrowStyle={{ borderRight: "7px solid  white" }}
+            iconStyle={{ background: "#5000ca", color: "rgb(39, 40, 34)" }}
             icon={<FontAwesomeIcon icon={faBriefcase} />}
           >
-            <h3 className="vertical-timeline-element-title"></h3>
-            <h4 className="vertical-timeline-element-subtitle"></h4>
+            <h3 className="vertical-timeline-element-title">
+              Senior Developer
+            </h3>
+            <h4 className="vertical-timeline-element-subtitle">Tech Company</h4>
           </VerticalTimelineElement>
           <VerticalTimelineElement
             className="vertical-timeline-element--work"
-            iconStyle={{ background: '#5000ca', color: 'rgb(39, 40, 34)' }}
+            iconStyle={{ background: "#5000ca", color: "rgb(39, 40, 34)" }}
             icon={<FontAwesomeIcon icon={faBriefcase} />}
           >
-            <h3 className="vertical-timeline-element-title"></h3>
-            <h4 className="vertical-timeline-element-subtitle"></h4>
+            <h3 className="vertical-timeline-element-title">
+              Software Engineer
+            </h3>
+            <h4 className="vertical-timeline-element-subtitle">Tech Startup</h4>
           </VerticalTimelineElement>
           <VerticalTimelineElement
             className="vertical-timeline-element--work"
-            iconStyle={{ background: '#5000ca', color: 'rgb(39, 40, 34)' }}
+            iconStyle={{ background: "#5000ca", color: "rgb(39, 40, 34)" }}
             icon={<FontAwesomeIcon icon={faBriefcase} />}
           >
-            <h3 className="vertical-timeline-element-title"></h3>
-            <h4 className="vertical-timeline-element-subtitle"></h4>
-            <p>
-            </p>
+            <h3 className="vertical-timeline-element-title">
+              Junior Developer
+            </h3>
+            <h4 className="vertical-timeline-element-subtitle">
+              Development Agency
+            </h4>
+            <p></p>
           </VerticalTimelineElement>
           <VerticalTimelineElement
             className="vertical-timeline-element--work"
-            iconStyle={{ background: '#5000ca', color: 'rgb(39, 40, 34)' }}
+            iconStyle={{ background: "#5000ca", color: "rgb(39, 40, 34)" }}
             icon={<FontAwesomeIcon icon={faBriefcase} />}
           >
-            <h3 className="vertical-timeline-element-title"></h3>
-            <h4 className="vertical-timeline-element-subtitle"></h4>
-            <p>
-            </p>
+            <h3 className="vertical-timeline-element-title">Intern</h3>
+            <h4 className="vertical-timeline-element-subtitle">
+              Local Tech Company
+            </h4>
+            <p></p>
           </VerticalTimelineElement>
         </VerticalTimeline>
       </div>
